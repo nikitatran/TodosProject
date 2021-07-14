1514094566eb52f4d06288364d303a9a25f9f7d5
Raymond Nazaryan <rayn2427@gmail.com>
2021-07-14 09:29:09 -0700

    fixed TodosRepository

--- src/main/java/com/cognixia/jump/repository/TodosRepository.java
+++ src/main/java/com/cognixia/jump/repository/TodosRepository.java
@@ -2,8 +2,8 @@ package com.cognixia.jump.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cognixia.jump.model.Todos;
+import com.cognixia.jump.model.Todo;
 
-public interface TodosRepository extends JpaRepository<User, Integer>{
+public interface TodosRepository extends JpaRepository<Todo, Integer>{
 
 }
