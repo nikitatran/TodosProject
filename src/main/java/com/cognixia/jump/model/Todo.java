8bb4f86d620533caf0f385c9ee4b0d990181e609
Nikita Tran <nikiusa09@yahoo.com>
2021-07-14 12:54:48 -0400

    added constructors, getters, setters, toString to todo model

--- src/main/java/com/cognixia/jump/model/Todo.java
+++ src/main/java/com/cognixia/jump/model/Todo.java
@@ -11,7 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -34,4 +33,58 @@ public class Todo implements Serializable{
 	@JoinColumn(name="user_id", referencedColumnName = "id")
 	@JsonBackReference
 	private User user;
+	
+	public Todo() {
+		this(-1, "N/A", new Date(System.currentTimeMillis()), new User());
+	}
+
+	public Todo(Integer id, @NotBlank String description, Date dueDate, User user) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.user = user;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", description=" + description + ", dueDate=" + dueDate + "]";
+	}
+	
 }
