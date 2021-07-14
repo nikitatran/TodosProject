a3b503f3a3333fbf5e561e9a2fbf28b9d61e4e51
Nikita Tran <nikiusa09@yahoo.com>
2021-07-14 12:22:25 -0400

    model created

---
+++ src/main/java/com/cognixia/jump/model/Todo.java
@@ -0,0 +1,28 @@
+package com.cognixia.jump.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Todo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotBlank
+	@Column(columnDefinition = "varchar(255)")
+	private String description;
+	
+	private Date dueDate;
+}
