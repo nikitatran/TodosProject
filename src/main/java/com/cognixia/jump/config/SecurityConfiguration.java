828d14bae525e12a509740c516bb24e6cc89a2fd
Nikita Tran <nikiusa09@yahoo.com>
2021-07-15 08:35:06 -0400

    added security using userdetailsservice

--- src/main/java/com/cognixia/jump/config/SecurityConfiguration.java
+++ src/main/java/com/cognixia/jump/config/SecurityConfiguration.java
@@ -1,34 +1,35 @@
 package com.cognixia.jump.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.cognixia.jump.service.MyUserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	UserDetailsService userDetailsService;
+	MyUserDetailsService userDetailsService;
 	
 	//configuration for the authentication (who are you?)
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+		/*
 		auth.inMemoryAuthentication()
 		.withUser("user1")
 		.password( passwordEncoder().encode("123") )
 		.roles("USER");
+		*/
 	}
 	
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+	/*public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+		//return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}*/
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
